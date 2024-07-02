6235b68ace29319f0d9c717035de6bec8b35ce79
Lucas Molozzi Mantelli <lucasmmantelli@gmail.com>
2024-07-02 10:59:37 -0300

    adiciona speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
-
+        
 class HomeTest(TestCase):
         def setUp(self):
             self.response=self.client.get(r('home')) 
@@ -8,16 +8,39 @@ class HomeTest(TestCase):
 
         def test_get(self):
             """ GET /  must return status code 200"""
-            response=self.client.get('/')
             self.assertEqual(200,self.response.status_code)
         
 
         def test_template(self):
             """Must use index.html"""
-            response=self.client.get('/')
             self.assertTemplateUsed(self.response,'index.html')    
         
 
         def test_subscription_link(self):
             expected = 'href="{}"'.format(r('subscriptions:new'))
             self.assertContains(self.response,expected)
+            
+        def test_speakers(self):
+            """Must show keynote speakers"""
+            contents =[
+                'Grace Hopper',
+                'http://hbn.link/hopper-pic',
+                'Alan Turing',
+                'http://hbn.link/turing-pic',
+            ]
+            for content in contents:
+                with self.subTest():
+                    self.assertContains(self.response,content)
+        
+        def test_speakers_link(self):
+            contents = [
+                'overview',
+                'speakers',
+                'sponsors',
+                'register',
+                'venue',
+            ]
+            for content in contents:
+                with self.subTest():
+                    expected = 'href="{}#{}"'.format(r('home'),content)
+                    self.assertContains(self.response, expected)
