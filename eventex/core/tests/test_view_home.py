08f41447605b46d6c9566060ecedb67cd3b111cc
Lucas Molozzi Mantelli <lucasmmantelli@gmail.com>
2024-07-03 09:13:04 -0300

    Speaker

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -2,45 +2,50 @@ from django.test import TestCase
 from django.shortcuts import resolve_url as r
         
 class HomeTest(TestCase):
-        def setUp(self):
-            self.response=self.client.get(r('home')) 
+    fixtures = ['keynotes.json']
+    def setUp(self):
+        self.response=self.client.get(r('home')) 
         
+    
 
-        def test_get(self):
-            """ GET /  must return status code 200"""
-            self.assertEqual(200,self.response.status_code)
-        
+    def test_get(self):
+        """ GET /  must return status code 200"""
+        self.assertEqual(200,self.response.status_code)
+    
 
-        def test_template(self):
-            """Must use index.html"""
-            self.assertTemplateUsed(self.response,'index.html')    
-        
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response,'index.html')    
+    
 
-        def test_subscription_link(self):
-            expected = 'href="{}"'.format(r('subscriptions:new'))
-            self.assertContains(self.response,expected)
-            
-        def test_speakers(self):
-            """Must show keynote speakers"""
-            contents =[
-                'Grace Hopper',
-                'http://hbn.link/hopper-pic',
-                'Alan Turing',
-                'http://hbn.link/turing-pic',
-            ]
-            for content in contents:
-                with self.subTest():
-                    self.assertContains(self.response,content)
+    def test_subscription_link(self):
+        expected = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response,expected)
         
-        def test_speakers_link(self):
-            contents = [
-                'overview',
-                'speakers',
-                'sponsors',
-                'register',
-                'venue',
-            ]
-            for content in contents:
-                with self.subTest():
-                    expected = 'href="{}#{}"'.format(r('home'),content)
-                    self.assertContains(self.response, expected)
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents =[
+            'href="{}"'.format(r("speaker_detail",slug = 'grace-hopper')),
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r("speaker_detail",slug = 'alan-turing')),
+            'Alan Turing',
+            'http://hbn.link/turing-pic',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertContains(self.response,content)
+    
+    def test_speakers_link(self):
+        contents = [
+            'overview',
+            'speakers',
+            'sponsors',
+            'register',
+            'venue',
+        ]
+        for content in contents:
+            with self.subTest():
+                expected = 'href="{}#{}"'.format(r('home'),content)
+                self.assertContains(self.response, expected)
+
